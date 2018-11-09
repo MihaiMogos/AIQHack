fd119a632e1d57520a74af13db8a9d03d152638d
Razvan Sandru <RazvanSandru93@gmail.com>
2018-11-09 21:00:39 +0200

    Edit homepage

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express()
 const port = 3000
 
 app.set('view engine', 'pug')
-app.get('/', (req, res) =>  res.render('index', { title: 'Hey', message: 'Hello there!' }))
+app.get('/', (req, res) =>  res.render('index', { title: 'BRObot' }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
